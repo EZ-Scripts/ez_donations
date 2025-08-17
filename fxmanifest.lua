72b79cc92c79602278d8a491ba19bc3812622fc3
Rayaan Uddin <rayaan.uddin@outlook.com>
2025-08-17 19:36:41 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,16 +4,18 @@ rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aw
 lua54 'yes'
 author 'Rayaan Uddin'
 description 'Tebex donation system for RedM using VORP'
-version '1.0.0'
+version '1.0'
+
+shared_script {'config.lua'}
 
 server_script {
     '@oxmysql/lib/MySQL.lua',
-    'config.lua',
-    'server.lua'
+    'sconfig.lua',
+    'server/*.lua'
 }
 
 client_script {
-    'client.lua'
+    'client/*.lua'
 }
 
 dependencies {
